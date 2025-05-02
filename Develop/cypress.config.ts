25fbc84402ee0735cf56f525e7082b7a00902721
gamalmubarak <eng-gemy@hotmail.com>
2025-05-02 15:05:54 -0400

    cypress.config created

---
+++ Develop/cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress';
+import customViteConfig from './vite.config';
+
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig: customViteConfig,
+    },
+    specPattern: "cypress/component/**/*.{js,jsx,ts,tsx}",
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
